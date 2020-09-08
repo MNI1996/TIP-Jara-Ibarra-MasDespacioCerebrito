c51c5f2d966f9876e3328e0d6b32dd22fc557045
IvanJara <ivanjaratamargo@gmail.com>
2020-09-07 22:15:38 -0300

    add initial webpack configuration, with loaders, development environment, hot reload, html and css bind and working

---
+++ frontend/build/webpack.config.dev.js
@@ -0,0 +1,52 @@
+'use strict'
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin  = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+
+  entry: [
+    './src/app.js'
+  ],
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // you cannot use MiniCssExtractPlugin with vue-style-loader at the same time
+          // 'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+}
