214acf1a770589fe1782dfb836a25ba3d198d6bb
IvanJara <ivanjaratamargo@gmail.com>
2020-09-08 23:06:38 -0300

    add router links in App.vue and router-view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <h1 class="main-title">Hello World!</h1>
+      <h1>Hello App!</h1>
+      <p>
+        <!-- use router-link component for navigation. -->
+        <!-- specify the link by passing the `to` prop. -->
+        <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+        <router-link :to="{name: 'home'}">Go to Home</router-link>
+        <router-link :to="{name: 'rooms'}">Go to Rooms</router-link>
+        <router-link :to="{name: 'profile'}">Go to Profile</router-link>
+      </p>
+      <!-- route outlet -->
+      <!-- component matched by the route will render here -->
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-name: "App"
+  name: "App"
 }
 </script>
