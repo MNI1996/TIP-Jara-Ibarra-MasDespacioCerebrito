23aeff44ea5773246e129d702ad21c8cbd2d61e6
IvanJara <ivanjaratamargo@gmail.com>
2020-09-10 20:32:54 -0300

    add post, and redefine get on QuestionApi

--- backend/src/api/questions.py
+++ backend/src/api/questions.py
@@ -1,5 +1,5 @@
 # flask packages
-from flask import jsonify
+from flask import jsonify, Response, request
 from flask_restful import Resource
 
 # mongo-engine models
@@ -7,6 +7,16 @@ from backend.src.model.Question import Question
 
 
 class QuestionApi(Resource):
-    def get(self):
+    @staticmethod
+    def get() -> Response:
         output = Question.objects()
         return jsonify({'result': output})
+
+    @staticmethod
+    def post() -> Response:
+        data = request.get_json()
+        post_question = Question(**data)
+        # agregar validacion
+        post_question.save()
+        return jsonify({'result': post_question})
+
