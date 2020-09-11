cb4e773c6873bba36159b985f10506551c40ae6b
IvanJara <ivanjaratamargo@gmail.com>
2020-09-10 21:33:42 -0300

    add Questions in the home, as an example for frontend-to-backend test

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,10 +1,27 @@
 <template>
-  <h1>This is the Home</h1>
+  <div>
+      <h1>This is the Home</h1>
+      <button @click="loadQuestions">Load Questions</button>
+      <h1>Questions</h1>
+      <ul>
+        <li v-for="question in questions">{{question.text}}</li>
+      </ul>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Home"
+  name: "Home",
+  computed:{
+    questions(){
+      return this.$store.getters.questions;
+    }
+  },
+  methods:{
+    loadQuestions(){
+        this.$store.dispatch("loadQuestions");
+    }
+  }
 }
 </script>
 
