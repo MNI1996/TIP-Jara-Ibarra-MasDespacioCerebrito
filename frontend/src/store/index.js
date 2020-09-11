d042d628eb969a98f0cd4b60fb25dd64f41d1ba4
IvanJara <ivanjaratamargo@gmail.com>
2020-09-10 21:32:22 -0300

    add url of the api questions, add state, getter, mutation and action for questions

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,11 +5,25 @@ import createLogger from "vuex/dist/logger";
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
+const apiUrl = "http://localhost:5000/questions/"
 
 export default new Vuex.Store({
   strict: debug,
   plugins: debug ? [createLogger()] : [],
   state:{
-    rooms: []
-  }
+    rooms: [],
+    questions: [],
+  },
+  getters:{
+    questions: (state) => state.questions,
+  },
+  mutations: {
+    setQuestions: (state, questions) => state.questions = questions,
+  },
+  actions: {
+    async loadQuestions({commit}){
+      let response = await Vue.axios.get(apiUrl);
+      commit('setQuestions', response.data.result)
+    }
+  },
 })
