befd7c6cca2959b8f72658a462b4870080e75f3d
IvanJara <ivanjaratamargo@gmail.com>
2020-09-12 02:51:16 -0300

    add Question.vue component, to show a question

---
+++ frontend/src/components/Question.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>{{question.text}}</h2>
+        <div>
+            <button v-for="option in question.options" class="btn-block" @click="answerQuestion(option)">{{ option.sentence }}</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Question",
+  props:{
+    question: Object,
+  },
+  data(){
+    return {
+      answered: false,
+    }
+  },
+  methods:{
+    answerQuestion(option){
+      this.$store.dispatch('answerQuestion',{questionId: this.question._id.$oid,option})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
