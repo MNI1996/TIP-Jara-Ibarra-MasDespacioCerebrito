2bdb8accb077360d073cbcdd70a4b8421221b1e4
IvanJara <ivanjaratamargo@gmail.com>
2020-09-12 02:48:40 -0300

    add PlayerApi to get player info and add players

--- backend/src/api/player.py
+++ backend/src/api/player.py
@@ -1,5 +1,5 @@
 # flask packages
-from flask import jsonify
+from flask import jsonify, Response, request
 from flask_restful import Resource
 
 # mongo-engine models
@@ -7,6 +7,14 @@ from backend.src.model.Player import Player
 
 
 class PlayerApi(Resource):
-    def get(self):
-        output = Player.objects()
+    @staticmethod
+    def get() -> Response:
+        output = Player.objects().first()
         return jsonify({'result': output})
+
+    @staticmethod
+    def post() -> Response:
+        data = request.get_json()
+        post_player = Player(**data)
+        post_player.save()
+        return jsonify({'result': post_player})
