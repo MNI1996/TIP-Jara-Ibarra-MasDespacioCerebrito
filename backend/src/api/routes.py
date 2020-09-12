27e3532059fb8ba98085353f1e5c83c925c2c53d
IvanJara <ivanjaratamargo@gmail.com>
2020-09-12 02:51:36 -0300

    add route for AnswerQuestionApi

--- backend/src/api/routes.py
+++ backend/src/api/routes.py
@@ -1,9 +1,10 @@
 from backend.src.api.room import RoomApi
 from backend.src.api.player import PlayerApi
-from backend.src.api.questions import QuestionApi
+from backend.src.api.questions import QuestionApi, AnswerQuestionApi
 
 
 def create_routes(api):
     api.add_resource(QuestionApi, '/questions/')
+    api.add_resource(AnswerQuestionApi, '/question/<id>/')
     api.add_resource(PlayerApi, '/players/')
     api.add_resource(RoomApi, '/rooms/')
