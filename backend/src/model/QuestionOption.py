58e7bd3ce1e0461ea561a68913451aa42664596d
IvanJara <ivanjaratamargo@gmail.com>
2020-09-12 02:51:57 -0300

    add Id to the QuestionOption.py

--- backend/src/model/QuestionOption.py
+++ backend/src/model/QuestionOption.py
@@ -1,6 +1,8 @@
-from mongoengine import StringField, BooleanField, EmbeddedDocument
+from bson import ObjectId
+from mongoengine import StringField, BooleanField, EmbeddedDocument, ObjectIdField
 
 
 class QuestionOption(EmbeddedDocument):
+    _id = ObjectIdField(required=True, default=lambda: ObjectId())
     sentence = StringField(required=True)
     correct = BooleanField(required=True, default=False)
