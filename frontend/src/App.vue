a7e0662c1bb9d0f8e3641cc22031c51f110d72ec
IvanJara <ivanjaratamargo@gmail.com>
2020-09-12 02:50:50 -0300

    Change title, hide unused router link, dispatch loadPlayer on created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-      <h1>Hello App!</h1>
+      <h1>Mas Despacio Cerebrito</h1>
       <p>
         <!-- use router-link component for navigation. -->
         <!-- specify the link by passing the `to` prop. -->
         <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
         <router-link :to="{name: 'home'}">Go to Home</router-link>
-        <router-link :to="{name: 'rooms'}">Go to Rooms</router-link>
+        <!--<router-link :to="{name: 'rooms'}">Go to Rooms</router-link>-->
         <router-link :to="{name: 'profile'}">Go to Profile</router-link>
       </p>
       <!-- route outlet -->
@@ -17,6 +17,9 @@
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  created() {
+    this.$store.dispatch('loadPlayer');
+  }
 }
 </script>
