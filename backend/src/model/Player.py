90cebc252b52afe083bea2a3d11d3cd36e1a918d
IvanJara <ivanjaratamargo@gmail.com>
2020-09-12 02:47:47 -0300

    add points to the Player model

--- backend/src/model/Player.py
+++ backend/src/model/Player.py
@@ -1,6 +1,6 @@
-from mongoengine import StringField, Document
+from mongoengine import StringField, Document, IntField
 
 
 class Player(Document):
-
     nick = StringField(required=True, primary_key=True)
+    points = IntField(default=0)
