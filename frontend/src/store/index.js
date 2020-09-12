35049ef8e2a2b2305fef3332a37843b83ba16bea
IvanJara <ivanjaratamargo@gmail.com>
2020-09-12 02:46:25 -0300

    add points, currentQuestion and player, state, getters,mutations and actions to load the values from the db

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,7 +5,7 @@ import createLogger from "vuex/dist/logger";
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
-const apiUrl = "http://localhost:5000/questions/"
+const apiUrl = "http://localhost:5000"
 
 export default new Vuex.Store({
   strict: debug,
@@ -13,17 +13,45 @@ export default new Vuex.Store({
   state:{
     rooms: [],
     questions: [],
+    points: 0,
+    currentQuestion: 0,
+    player: null,
   },
   getters:{
     questions: (state) => state.questions,
+    currentQuestion: (state) => state.currentQuestion,
+    points: (state) => state.points,
+    player: (state) => state.player,
   },
   mutations: {
     setQuestions: (state, questions) => state.questions = questions,
+    setPlayer: (state, player) => state.player = player,
+    addPoint: (state, answer) => {
+      if(answer){
+        state.points++;
+      }
+    },
+    nextQuestion: (state) => {
+        state.currentQuestion++;
+    },
   },
   actions: {
     async loadQuestions({commit}){
-      let response = await Vue.axios.get(apiUrl);
+      let response = await Vue.axios.get(apiUrl+"/questions/");
       commit('setQuestions', response.data.result)
-    }
+    },
+    async answerQuestion({commit}, {questionId, option}){
+      let data = {
+        id: option._id.$oid,
+        sentence: option.sentence,
+      }
+      let response = await Vue.axios.post(apiUrl+"/question/"+questionId+"/",{data:data});
+      commit('addPoint', response.data.result)
+      commit('nextQuestion')
+    },
+    async loadPlayer({commit}){
+      let response = await Vue.axios.get(apiUrl+"/players/");
+      commit('setPlayer', response.data.result)
+    },
   },
 })
