3e62bc9491394f26d7040323f6c3748a32446ac1
IvanJara <ivanjaratamargo@gmail.com>
2020-09-28 09:41:27 -0300

    add html to display rooms, add button to create a room that send the user to create_room

--- frontend/src/pages/Rooms.vue
+++ frontend/src/pages/Rooms.vue
@@ -1,10 +1,33 @@
 <template>
-  <h1>This is rooms</h1>
+  <div>
+    <template v-if="rooms.length > 0">
+      <h1>Those are all the rooms available now</h1>
+      <room-card v-for="room in rooms" />
+    </template>
+    <template v-else>
+      <h1>There are no rooms created right now</h1>
+    </template>
+    <button @click="goToCreateARoom" class="btn btn-lg btn-success">Create a Room</button>
+  </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+import RoomCard from "../components/RoomCard.vue";
+
 export default {
-name: "Rooms"
+name: "Rooms",
+  components:{
+    RoomCard
+  },
+  computed:{
+    ...mapGetters(["rooms"]),
+  },
+  methods:{
+    goToCreateARoom(){
+     this.$router.push({name: "create_room"})
+    }
+  }
 }
 </script>
 
