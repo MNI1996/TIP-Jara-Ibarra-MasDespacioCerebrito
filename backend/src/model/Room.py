8b7fa6b51d1b9dd427ca3c906bdf264436720f7c
IvanJara <ivanjaratamargo@gmail.com>
2020-09-28 11:05:09 -0300

    add __str__ for a Room

--- backend/src/model/Room.py
+++ backend/src/model/Room.py
@@ -3,7 +3,9 @@ from backend.src.model.Round import Round
 
 
 class Room(Document):
-
-    id= IntField(primary_key=True)
-    participants = IntField
+    id = IntField(primary_key=True)
+    participants = IntField(default=1)
     #round=EmbeddedDocumentListField(Round,default=[])
+
+    def __str__(self):
+        return f"ID: {self.id}, participants: {self.participants}"
