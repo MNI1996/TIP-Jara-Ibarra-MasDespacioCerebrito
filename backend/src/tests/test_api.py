68fa56e87199a30176066ef8e7ead8897b965f15
IvanJara <ivanjaratamargo@gmail.com>
2020-10-02 22:44:32 -0300

    add test for questions post, add setup and teardown

--- backend/src/tests/test_api.py
+++ backend/src/tests/test_api.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.case import TestCase
-
+from mongoengine import connect, disconnect
 import app
 
 test_config = {
@@ -11,21 +11,43 @@ test_config = {
         'port': 27017,
         'username': 'admin',
         'password': 'password',
-        'authentication_source': 'admin'
+        'authentication_source': 'admin',
+        'alias': 'testing_db',
+        'is_mock': True,
     }
 }
 
 
 class TestApiQuestions(TestCase):
 
+    def setUp(self):
+        self.test_app = app.get_flask_app(test_config)
+        self.test_app.testing = True
+        self.test_app.debug = True
+        self.test_client = self.test_app.test_client()
+        connect('testing_db', host='mongomock://localhost')
+
     def test_connection_on_questions_get_and_empty_result(self):
-        test_app = app.get_flask_app(test_config)
-        test_app.testing = True
-        test_app.debug = True
-        test_client = test_app.test_client()
-        response = test_client.get("/questions/")
+        response = self.test_client.get("/questions/")
         self.assertEqual(200, response.status_code)
         self.assertEqual([], response.json["result"])
+
+    def test_create_a_question(self):
+        data = {"text": "Las 3 carabelas eran: la pinta, la niña y ...",
+                "options": [{"sentence": "Santa Martina"},
+                            {"sentence": "Santa Marina"},
+                            {"sentence": "Santa Maria", "correct": "True"}
+                            ]}
+        response = self.test_client.post("/questions/", json=data)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(data["text"], response.json['result']['text'])
+
+        response2 = self.test_client.get("/questions/")
+        self.assertEqual(1, len(response2.json["result"]))
+        self.assertEqual(data["text"], response2.json["result"][0]['text'])
+
+    def tearDown(self):
+        disconnect('testing_db')
 
 
 if __name__ == '__main__':
