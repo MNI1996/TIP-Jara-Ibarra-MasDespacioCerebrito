3a3af2e0986da0d89b75011cad3d491eb79fd44e
IvanJara <ivanjaratamargo@gmail.com>
2020-10-13 18:50:30 -0300

    add post for create a room, fix room api get

--- backend/src/api/room.py
+++ backend/src/api/room.py
@@ -1,10 +1,11 @@
 # flask packages
-from flask import jsonify
+from flask import jsonify, Response, request
 from flask_restful import Resource, abort
 
 # mongo-engine models
-from mongoengine import DoesNotExist
+from mongoengine import DoesNotExist, ValidationError
 
+from backend.src.model.Player import Player
 from backend.src.model.Room import Room
 
 
@@ -13,11 +14,26 @@ class RoomsApi(Resource):
         output = Room.objects()
         return jsonify({'result': output})
 
+    @staticmethod
+    def post() -> Response:
+        owner_name = request.form['owner']
+        room_name = request.form['name']
+        try:
+            player = Player.objects.get(nick=owner_name)
+        except DoesNotExist:
+            raise abort(404)
+        post_room = Room(name=room_name, owner=player)
+        try:
+            post_room.save()
+        except ValidationError as e:
+            raise abort(400, message=e.message)
+        return jsonify({'result': post_room})
+
 
 class RoomApi(Resource):
     def get(self, room_id):
         try:
-            output = Room.objects.get(id=room_id)
+            output = Room.objects.get(name=room_id)
         except DoesNotExist:
             raise abort(404)
         return jsonify({'result': output})
