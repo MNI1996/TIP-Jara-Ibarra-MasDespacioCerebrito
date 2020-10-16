050d5e3b610aa76c8b37c26f5f963c8b9a11531d
IvanJara <ivanjaratamargo@gmail.com>
2020-10-16 20:59:00 -0300

    add Category document with a name, and in Room add a list of categories

--- backend/src/model/Room.py
+++ backend/src/model/Room.py
@@ -3,6 +3,10 @@ from mongoengine import IntField, Document, ReferenceField, ListField, QuerySet,
 from backend.src.model.Player import Player
 
 
+class Category(Document):
+    name = StringField(primary_key=True)
+
+
 # TODO: Rename to RoomManager and extract to another file
 class RoomQuerySet(QuerySet):
     def add_participant(self, room_id, a_participant):
@@ -17,6 +21,7 @@ class Room(Document):
     owner = ReferenceField(Player)
     participants = ListField(ReferenceField(Player), default=[])
     rounds = IntField(default=5)
+    categories = ListField(ReferenceField(Category), default=[])
     meta = {'queryset_class': RoomQuerySet}
 
     def __str__(self):
