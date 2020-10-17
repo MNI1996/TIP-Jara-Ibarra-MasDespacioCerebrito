2b7f98e677bfa24acb29dda1410ae13560a7d7f8
IvanJara <ivanjaratamargo@gmail.com>
2020-10-17 02:53:46 -0300

    rename parameter, change new field name

--- backend/src/model/Room.py
+++ backend/src/model/Room.py
@@ -9,9 +9,8 @@ class Category(Document):
 
 # TODO: Rename to RoomManager and extract to another file
 class RoomQuerySet(QuerySet):
-    def add_participant(self, room_id, a_participant):
-        print(id, a_participant, flush=True)
-        a_room = Room.objects(id=room_id).first()
+    def add_participant(self, room_name, a_participant):
+        a_room = Room.objects(name=room_name).first()
         if a_room.owner.nick != a_participant.nick:
             a_room.update(add_to_set__participants=a_participant)
 
