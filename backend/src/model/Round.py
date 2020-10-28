6ed4824d7980ff20eea1fc185b6dfcefaf8de18d
IvanJara <ivanjaratamargo@gmail.com>
2020-10-28 01:41:12 -0300

    refactor Round Document

--- backend/src/model/Round.py
+++ backend/src/model/Round.py
@@ -1,10 +1,12 @@
+from mongoengine import Document, ReferenceField, ListField
+
+from backend.src.model.Answer import Answer
+from backend.src.model.Question import Question
+from backend.src.model.Room import Room
 
 
+class Round(Document):
+    room = ReferenceField(Room)
+    question = ReferenceField(Question)
+    answers = ListField(ReferenceField(Answer), default=[])
 
-class Round:
-    def __init__(self, question, point=5):
-        self.question = question
-        self.point = point
-
-    def resolve(self, answer):
-        return self.question.options[answer].valuate()
