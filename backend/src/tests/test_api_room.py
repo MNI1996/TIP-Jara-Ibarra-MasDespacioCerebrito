cfb2a1720d1a177274164b5d3fa53931cb637644
IvanJara <ivanjaratamargo@gmail.com>
2020-10-30 01:04:18 -0300

    add test for room default rounds_amount, add test for 1 round creation of a room, and rounds_amount counter

--- backend/src/tests/test_api_room.py
+++ backend/src/tests/test_api_room.py
@@ -3,6 +3,7 @@ from unittest.case import TestCase
 from mongoengine import connect, disconnect
 import app
 from backend.src.model.Player import Player
+from backend.src.model.Question import Question
 from backend.src.model.Room import Room, Category
 
 test_config = {
@@ -91,6 +92,51 @@ class TestApiRoom(TestCase):
         self.assertEqual(player.nick, response_2.json['result']['owner'])
         self.assertEqual(room_data_2['name'], response_2.json['result']['_id'])
         self.assertEqual(room_data_2['categories'], response_2.json['result']['categories'])
+
+    def test_08_create_a_room_default_amount_value(self):
+        player = Player(nick="Juan")
+        player.save()
+        room_data_2 = {'owner': "Juan",
+                       'name': "Sala 3",
+                       }
+        response_2 = self.test_client.post("/rooms/", json=room_data_2)
+        self.assertEqual(200, response_2.status_code)
+        self.assertEqual(5, response_2.json['result']['rounds_amount'])
+
+    def test_09_create_a_room_with_1_rounds_amount_set_it_properly(self):
+        player = Player(nick="Juan")
+        player.save()
+        room_data_2 = {'owner': "Juan",
+                       'name': "Sala 3",
+                       'rounds_amount': 1,
+                       }
+        response_2 = self.test_client.post("/rooms/", json=room_data_2)
+        self.assertEqual(200, response_2.status_code)
+        self.assertEqual(1, response_2.json['result']['rounds_amount'])
+
+    def test_10_create_a_room_with_1_round_creates_a_round_object(self):
+        art_category = Category(name="Art")
+        art_category.save()
+        data = {"text": "Las 3 carabelas eran: la pinta, la niña y ...",
+                "options": [{"sentence": "Santa Martina"},
+                            {"sentence": "Santa Marina"},
+                            {"sentence": "Santa Maria", "correct": "True"}
+                            ],
+                "categories": ["Art"]}
+        question = Question(**data)
+        question.save()
+        player = Player(nick="Juan")
+        player.save()
+        room_data_2 = {'owner': "Juan",
+                       'name': "Sala 3",
+                       'rounds_amount': 1,
+                       'categories': ['Art']
+                       }
+        response_2 = self.test_client.post("/rooms/", json=room_data_2)
+        self.assertEqual(200, response_2.status_code)
+        self.assertEqual(1, response_2.json['result']['rounds_amount'])
+        self.assertEqual(1, len(Room.objects.get(name=response_2.json['result']['_id'])['rounds']))
+
 
     def tearDown(self):
         disconnect('testing_db')
