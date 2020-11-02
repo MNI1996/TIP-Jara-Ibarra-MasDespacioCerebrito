b099bac356f997e72fcf59ce6dbde9f311e71aea
IvanJara <ivanjaratamargo@gmail.com>
2020-11-02 10:09:17 -0300

    add difficulty to the Question document

--- backend/src/model/Question.py
+++ backend/src/model/Question.py
@@ -1,4 +1,4 @@
-from mongoengine import Document, StringField, EmbeddedDocumentListField, ListField, ReferenceField
+from mongoengine import Document, StringField, EmbeddedDocumentListField, ListField, ReferenceField, IntField
 
 from backend.src.model.Category import Category
 from backend.src.model.QuestionOption import QuestionOption
@@ -8,3 +8,4 @@ class Question(Document):
     text = StringField(required=True)
     options = EmbeddedDocumentListField(QuestionOption, default=[])
     categories = ListField(ReferenceField(Category), default=[])
+    difficulty = IntField(default=1)
