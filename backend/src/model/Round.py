90ab4af76168ca86f747abaeecdc69b0235096f2
IvanJara <ivanjaratamargo@gmail.com>
2020-11-05 21:39:43 -0300

    add an auto generated id to the round

--- backend/src/model/Round.py
+++ backend/src/model/Round.py
@@ -1,10 +1,12 @@
-from mongoengine import ReferenceField, EmbeddedDocument, EmbeddedDocumentListField
+from bson import ObjectId
+from mongoengine import ReferenceField, EmbeddedDocument, EmbeddedDocumentListField, ObjectIdField
 
 from backend.src.model.Answer import Answer
 from backend.src.model.Question import Question
 
 
 class Round(EmbeddedDocument):
+    id = ObjectIdField(required=True, default=lambda: ObjectId(), primary_key=True)
     question = ReferenceField(Question)
     answers = EmbeddedDocumentListField(Answer, default=[])
 
