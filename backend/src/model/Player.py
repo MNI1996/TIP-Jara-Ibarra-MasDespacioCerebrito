55d16ff0fb46905353877ec56a816a957aa0c73c
IvanJara <ivanjaratamargo@gmail.com>
2020-11-07 01:29:41 -0300

    add min length validation for the nick

--- backend/src/model/Player.py
+++ backend/src/model/Player.py
@@ -2,5 +2,5 @@ from mongoengine import StringField, Document, IntField
 
 
 class Player(Document):
-    nick = StringField(required=True, primary_key=True)
+    nick = StringField(required=True, primary_key=True, min_length=3)
     points = IntField(default=0)
