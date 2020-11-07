e4e4a824d9088f78c73a7f640b5d2fa5e1055658
IvanJara <ivanjaratamargo@gmail.com>
2020-11-07 02:07:17 -0300

    add /ranking/players/ in the routes

--- backend/src/api/routes.py
+++ backend/src/api/routes.py
@@ -1,3 +1,4 @@
+from backend.src.api.ranking import RankingApi
 from backend.src.api.room import RoomsApi, RoomApi
 from backend.src.api.player import PlayerApi
 from backend.src.api.questions import QuestionApi, AnswerQuestionApi
@@ -9,3 +10,4 @@ def create_routes(api):
     api.add_resource(PlayerApi, '/players/')
     api.add_resource(RoomsApi, '/rooms/')
     api.add_resource(RoomApi, '/rooms/<room_id>/')
+    api.add_resource(RankingApi, '/ranking/players/')
