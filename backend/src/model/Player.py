d1a0e87caf5891015c8be0958ff6186f12cd34d8
IvanJara <ivanjaratamargo@gmail.com>
2020-11-19 21:08:57 -0300

    add password field to the Player

--- backend/src/model/Player.py
+++ backend/src/model/Player.py
@@ -4,3 +4,4 @@ from mongoengine import StringField, Document, IntField
 class Player(Document):
     nick = StringField(required=True, primary_key=True, min_length=3)
     points = IntField(default=0)
+    password = StringField(min_length=6)
