ceaf63222651fc21be7ca5d9af81b7f622573a6b
IvanJara <ivanjaratamargo@gmail.com>
2020-11-21 22:39:51 -0300

    add password input, rearrange html, add responsive

--- frontend/src/components/UserLogin.vue
+++ frontend/src/components/UserLogin.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>
-    <label style="color: aliceblue">
-      Elija su Nombre
-      <input v-model="nick" type="text" maxlength="30">
-    </label>
-    <button @click="login" class="btn btn-lg btn-success">Ingresar</button>
+  <div class="col">
+    <div class="row justify-content-center">
+      <div class="col-6 col-md-2 text-right">
+        <label for="nick" class="letra">Usuario</label>
+      </div>
+      <div class="col-6 col-md-2">
+        <input v-model="nick" id="nick" type="text" maxlength="30">
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-6 col-md-2 text-right">
+        <label for="pass" class="letra">Contraseña</label>
+      </div>
+      <div class="col-6 col-md-2">
+        <input v-model="password" id="pass" type="password" maxlength="30">
+      </div>
+    </div>
+    <div class="row boton-ingresar">
+      <button @click="login" class="btn btn-lg btn-success">Ingresar</button>
+    </div>
   </div>
 </template>
 
@@ -14,16 +28,17 @@ export default {
   data() {
     return {
       nick: "",
+      password: "",
     }
   },
-  methods:{
-    login(){
-      this.$store.dispatch('login', this.nick);
+  methods: {
+    login() {
+      let data = {
+        nick: this.nick,
+        password: this.password,
+      }
+      this.$store.dispatch('login', data);
     }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
