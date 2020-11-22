3cbf18fb10dec8d2e4abe10fd6fe7ac460a61930
IvanJara <ivanjaratamargo@gmail.com>
2020-11-21 22:39:00 -0300

    add new component Authentication.vue that wraps register and user-login components and change between the 2 cases

---
+++ frontend/src/components/Authentication.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="col authentication">
+    <register v-if="register" />
+    <user-login v-else />
+    <div class="row register-or-login">
+        <button @click="toggleRegister" class="btn btn-lg btn-primary">{{registerOrLogin}}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserLogin from "./UserLogin.vue";
+import Register from "./Register.vue";
+
+export default {
+  name: "Authentication",
+  components: {Register, UserLogin},
+  data() {
+    return {
+      register: false,
+    }
+  },
+  computed:{
+    registerOrLogin(){
+      return this.register ? "Ya tengo Usuario" : "No tengo usuario";
+    }
+  },
+  methods:{
+    toggleRegister(){
+      this.register = !this.register;
+    }
+  }
+}
+</script>
