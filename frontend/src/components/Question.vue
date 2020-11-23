395e6001a6fb723f29996cd2cbd3d042d39eea6b
IvanJara <ivanjaratamargo@gmail.com>
2020-11-23 16:01:00 -0300

    add Tiempo in the html, add time logic, replace answerOption way to pick the option, add method start round

--- frontend/src/components/Question.vue
+++ frontend/src/components/Question.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <h2 class="letra">Tiempo: {{countdown}}</h2>
         <h2 class="letra">{{question.text}}</h2>
         <div class="col justify-content-center">
           <div :class="{correct: option.correct, incorrect: !option.correct, active: isSelected(option)}"
@@ -24,14 +25,20 @@ export default {
     return {
       answered: false,
       selected: null,
+      roundTime: 10,
+      currentTime: 0,
     }
   },
   computed: {
      ...mapGetters(["currentRoom"]),
+    countdown(){
+       return this.roundTime - this.currentTime;
+    }
   },
   methods:{
-    async answerQuestion(option){
-      await this.$store.dispatch('answerQuestion',{questionId: this.question._id.$oid,option})
+    async answerQuestion(){
+      let selectedOption = this.selected;
+      await this.$store.dispatch('answerQuestion',{questionId: this.question._id.$oid, option: selectedOption})
       this.$parent.$parent.socket.emit('player_answered', {room:this.currentRoom._id, question_id: this.question._id.$oid});
     },
     isSelected(option){
@@ -39,6 +46,23 @@ export default {
     },
     selectOption(option){
       this.selected = option;
+    },
+    startRound(){
+      setTimeout(() => {
+        if(this.currentTime >= this.roundTime){
+          this.answerQuestion();
+          this.$parent.$parent.endRoundForOwner()
+          this.currentTime = 0;
+          this.selected = null;
+          return ;
+        }
+        this.addOne();
+        this.startRound();
+    }, 1000);
+
+    },
+    addOne(){
+      this.currentTime = this.currentTime + 1;
     }
   }
 }
