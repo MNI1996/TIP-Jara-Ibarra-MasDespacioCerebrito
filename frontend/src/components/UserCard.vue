c1d05f63b6bbff2696c0d140f544dbc3e6d0679a
Ivan <ivanjaratamargo@gmail.com>
2020-11-26 01:29:20 -0300

    add class

--- frontend/src/components/UserCard.vue
+++ frontend/src/components/UserCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="user-card">
-    <img src="Images/Profile.png" alt="" class="img-fluid ">
+    <img src="Images/Profile.png" alt="" class="img-fluid user-card-img">
     <p>{{dato}}</p>
   </div>
 </template>
