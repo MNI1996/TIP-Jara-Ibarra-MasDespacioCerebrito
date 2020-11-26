79b176384e2267f8f8c38feeece0810bf93db512
Ivan <ivanjaratamargo@gmail.com>
2020-11-26 01:29:11 -0300

    fix responsive on NotStartedGame.vue

--- frontend/src/components/NotStartedGame.vue
+++ frontend/src/components/NotStartedGame.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="not-started-game-container">
-    <div class="row col-md-10 offset-md-1 welcome">
+    <div class="row col-12 col-md-10 offset-md-1 welcome">
       <img src="Images/jackpot.png" class="img-fluid welcome-logo-start">
-      <h1>Bienvenido a {{ currentRoom._id }}</h1>
+      <h1>Bienvenido a <br class="d-block d-sm-none"> {{ currentRoom._id }}</h1>
       <img src="Images/jackpot.png" class="img-fluid welcome-logo-end">
     </div>
-    <div class="row col-md-10 offset-md-1 game-information">
-      <div class="col-md-7">
+    <div class="row col-12 col-md-10 offset-md-1 game-information">
+      <div class="col-12 col-md-7">
         <div class="row">
-          <div class="col-md-3 game-info-div">
+          <div class="col-12 col-md-3 game-info-div">
             <div class="row">
               <h2>Rondas</h2>
             </div>
@@ -16,7 +16,7 @@
               <h2>{{ currentRoom.rounds.length }}</h2>
             </div>
           </div>
-          <div class="col-md-8 offset-md-1 game-info-div">
+          <div class="col-12 col-md-8 offset-md-1 game-info-div">
             <div class="row">
               <h2>Tiempo por ronda</h2>
             </div>
@@ -30,20 +30,20 @@
             <h2>Jugadores en la Sala</h2>
           </div>
           <div v-if="currentRoom.participants && currentRoom.participants.length > 0" class="col-md-12 row">
-            <user-card v-for="participant in this.currentRoom.participants" :dato="participant" class="col-md-2"/>
+            <user-card v-for="participant in this.currentRoom.participants" :dato="participant" class="col-4 col-md-2"/>
           </div>
         </div>
       </div>
-      <div class="col-md-5 ">
+      <div class="col-12 col-md-5 info-categories-container">
         <div class="info-categories">
           <div class="col-12">
             <h2>Categorias</h2>
           </div>
           <div class="col-12" style="align-content: center">
             <div class="row">
-              <div v-for="i in this.currentRoom.categories" class="col-md-3">
-                <img :src="generateUrl(i)" alt="" class="img-fluid category-img">
-                <p style="color: aliceblue;">{{ i }}</p>
+              <div v-for="i in this.currentRoom.categories" class="col-4 col-md-3 center-card cat-card-creation">
+                <p>{{ i }}</p>
+                <img :src="generateUrl(i)" alt="" class="img-fluid cat-card-img">
               </div>
             </div>
           </div>
