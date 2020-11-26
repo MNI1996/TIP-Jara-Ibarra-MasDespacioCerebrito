564d8f112147d03682bb6c6a0f1dc9db63e8834d
Ivan <ivanjaratamargo@gmail.com>
2020-11-25 22:27:20 -0300

    add class to fix badges sizes

--- frontend/src/components/GameState.vue
+++ frontend/src/components/GameState.vue
@@ -2,9 +2,9 @@
   <div class="fixed-bottom game-state-container">
     <div class="col-md-4 offset-md-4 row game-state-row" v-for="(state,index) in gameState">
         <div class="col-md-2 text-center">
-          <img v-if="index === 0 && state.points > 0" class="img-fluid" src="Images/trophy.png">
-          <img v-if="index === 1 && state.points > 0" class="img-fluid" src="Images/silver-medal.png">
-          <img v-if="index === 2 && state.points > 0" class="img-fluid" src="Images/bronze-medal.png">
+          <img v-if="index === 0 && state.points > 0" class="img-fluid state-img" src="Images/trophy.png">
+          <img v-if="index === 1 && state.points > 0" class="img-fluid state-img" src="Images/silver-medal.png">
+          <img v-if="index === 2 && state.points > 0" class="img-fluid state-img" src="Images/bronze-medal.png">
         </div>
         <div class="col-md-6 text-center"> {{state.player}}</div>
         <div class="col-md-4 text-center"> {{state.points}}</div>
