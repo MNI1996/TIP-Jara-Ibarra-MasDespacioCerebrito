2355c9147988c483d6369661616a89dd7f4c222a
Ivan <ivanjaratamargo@gmail.com>
2020-11-28 07:41:12 -0300

    fix responsive for GameState

--- frontend/src/components/GameState.vue
+++ frontend/src/components/GameState.vue
@@ -1,13 +1,13 @@
 <template>
   <div :class="{'fixed-bottom': !gameEnded}" class="game-state-container">
-    <div class="col-md-4 offset-md-4 row game-state-row" v-for="(state,index) in gameState.slice(0,rowsAmount)">
-        <div class="col-md-2 text-center">
+    <div class="col-10 offset-1 col-md-4 offset-md-4 row game-state-row" v-for="(state,index) in gameState.slice(0,rowsAmount)">
+        <div class="col-2 col-md-2 text-center">
           <img v-if="index === 0 && state.points > 0" class="img-fluid state-img" src="Images/trophy.png">
           <img v-if="index === 1 && state.points > 0" class="img-fluid state-img" src="Images/silver-medal.png">
           <img v-if="index === 2 && state.points > 0" class="img-fluid state-img" src="Images/bronze-medal.png">
         </div>
-        <div class="col-md-6 text-center"> {{state.player}}</div>
-        <div class="col-md-4 text-center"> {{state.points}}</div>
+        <div class="col-6 col-md-6 text-center"> {{state.player}}</div>
+        <div class="col-4 col-md-4 text-center"> {{state.points}}</div>
     </div>
   </div>
 </template>
