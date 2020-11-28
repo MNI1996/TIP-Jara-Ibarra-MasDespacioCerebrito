bfe8d4fc2702cadff2acb91339f8df286b95bd4f
Ivan <ivanjaratamargo@gmail.com>
2020-11-28 07:08:05 -0300

    add props and dynamic style on GameState.vue

--- frontend/src/components/GameState.vue
+++ frontend/src/components/GameState.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="fixed-bottom game-state-container">
-    <div class="col-md-4 offset-md-4 row game-state-row" v-for="(state,index) in gameState.slice(0,3)">
+  <div :class="{'fixed-bottom': !gameEnded}" class="game-state-container">
+    <div class="col-md-4 offset-md-4 row game-state-row" v-for="(state,index) in gameState.slice(0,rowsAmount)">
         <div class="col-md-2 text-center">
           <img v-if="index === 0 && state.points > 0" class="img-fluid state-img" src="Images/trophy.png">
           <img v-if="index === 1 && state.points > 0" class="img-fluid state-img" src="Images/silver-medal.png">
@@ -16,7 +16,13 @@
 export default {
   name: "GameState",
   props: {
-    gameState: {type: Array}
+    gameState: {type: Array},
+    gameEnded: Boolean,
   },
+  computed:{
+    rowsAmount(){
+      return this.gameEnded? this.gameEnded.length : 3;
+    }
+  }
 }
 </script>
