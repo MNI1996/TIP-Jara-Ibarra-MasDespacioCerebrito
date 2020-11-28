75c133c3d023f2c564969b7c4d9d205368c448e7
Ivan <ivanjaratamargo@gmail.com>
2020-11-28 00:11:32 -0300

    add input validation on UserLogin.vue and Register.vue

--- frontend/src/components/UserLogin.vue
+++ frontend/src/components/UserLogin.vue
@@ -5,7 +5,7 @@
         <label for="nick" class="letra">Usuario</label>
       </div>
       <div class="col-6 col-md-4 col-lg-3">
-        <input v-model="nick" id="nick" type="text" maxlength="30">
+        <input v-model="nick" id="nick" type="text" maxlength="30" minlength="3">
       </div>
     </div>
     <div class="row justify-content-start">
@@ -13,11 +13,15 @@
         <label for="pass" class="letra">Contraseña</label>
       </div>
       <div class="col-6 col-md-4 col-lg-3">
-        <input v-model="password" id="pass" type="password" maxlength="30">
+        <input v-model="password" id="pass" type="password" maxlength="30" minlength="6">
       </div>
     </div>
+    <div class="col-12 col-md-8 col-lg-6 justify-content-start">
+      <p class="invalid-message" v-if="!validNick">El usuario tiene que tener al menos 3 caracteres</p>
+      <p class="invalid-message" v-if="!validPassword">La contraseña tiene que tener al menos 6 caracteres</p>
+    </div>
     <div class="boton-ingresar col-12 col-md-4 offset-md-4 col-lg-3 offset-lg-3">
-      <button @click="login" class="btn btn-lg btn-success">Ingresar</button>
+      <button @click="login" class="btn btn-lg btn-success" :disabled="anyFieldInvalid">Ingresar</button>
     </div>
   </div>
 </template>
@@ -30,6 +34,17 @@ export default {
       nick: "",
       password: "",
     }
+  },
+  computed:{
+    anyFieldInvalid(){
+      return !this.validNick || !this.validPassword
+    },
+    validNick(){
+      return this.nick.length >= 3;
+    },
+    validPassword(){
+      return this.password.length >= 6;
+    },
   },
   methods: {
     login() {
