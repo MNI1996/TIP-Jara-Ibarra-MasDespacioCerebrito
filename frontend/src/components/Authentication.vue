bc289fc8a16ff38f28922bda2a8328ff21889a4d
Ivan <ivanjaratamargo@gmail.com>
2020-12-02 21:07:38 -0300

    fix col sizes

--- frontend/src/components/Authentication.vue
+++ frontend/src/components/Authentication.vue
@@ -2,7 +2,7 @@
   <div class="col-12 col-md-4 offset-md-1 authentication darkslate-panel">
     <register v-if="register" />
     <user-login v-else />
-    <div class="register-or-login col-12 col-md-4 offset-md-4 col-lg-3 offset-lg-3">
+    <div class="register-or-login col-12">
         <button @click="toggleRegister" class="btn btn-lg btn-primary">{{registerOrLogin}}</button>
     </div>
   </div>
