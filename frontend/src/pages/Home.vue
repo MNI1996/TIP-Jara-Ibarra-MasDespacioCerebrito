110c6aee410ade0dc4029abadd9a32c0ab489c1a
Ivan <ivanjaratamargo@gmail.com>
2020-12-02 21:06:09 -0300

    move classes to Rooms.vue, delete logo-background class

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="row home-container">
-    <div class="col-10 col-md-4 logo-background">
+    <div class="col-10 col-md-5">
       <img src="Images/Logo.png" alt="Brainy" class="img-fluid logo-home">
     </div>
     <authentication v-if="!player"/>
-    <rooms id="rooms" class="col-md-8" v-if="logged"/>
+    <rooms id="rooms" v-if="logged"/>
   </div>
 </template>
 
