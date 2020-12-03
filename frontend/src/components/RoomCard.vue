1c4927155c8f45158a364f0fc788401f1b5963a5
Ivan <ivanjaratamargo@gmail.com>
2020-12-02 21:07:22 -0300

    replace css class, fix col sizes

--- frontend/src/components/RoomCard.vue
+++ frontend/src/components/RoomCard.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="row player-info-card">
-    <div class="ranking-points col col-md-3"><h3>{{ room._id }}</h3></div>
-    <div class="ranking-points col col-md-3"><h3>{{ this.room["owner"] }}</h3></div>
-    <div class="ranking-points col col-md-3"><h3>{{ this.room["participants"].length }}</h3></div>
-    <div class="ranking-points col col-md-2">
-      <button @click="goToRoom" class="btn btn-success">Unirse</button>
+  <div class="row">
+    <div class="col-11 room-card">
+      <div class="row">
+        <div class="ranking-points col col-md-5"><h3>{{ room._id }}</h3></div>
+        <div class="ranking-points col col-md-2"><h3>{{ this.room["owner"] }}</h3></div>
+        <div class="ranking-points col col-md-2"><h3>{{ this.room["participants"].length }}</h3></div>
+        <div class="ranking-points col col-md-2">
+          <button @click="goToRoom" class="btn btn-success">Unirse</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
