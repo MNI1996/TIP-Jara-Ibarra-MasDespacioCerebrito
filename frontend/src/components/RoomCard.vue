d3981004f9dea1233d385366074d783c67403573
Ivan <ivanjaratamargo@gmail.com>
2020-12-02 21:38:53 -0300

    fix sizes

--- frontend/src/components/RoomCard.vue
+++ frontend/src/components/RoomCard.vue
@@ -4,8 +4,8 @@
       <div class="row">
         <div class="ranking-points col col-md-5"><h3>{{ room._id }}</h3></div>
         <div class="ranking-points col col-md-2"><h3>{{ this.room["owner"] }}</h3></div>
-        <div class="ranking-points col col-md-2"><h3>{{ this.room["participants"].length }}</h3></div>
-        <div class="ranking-points col col-md-2">
+        <div class="ranking-points col col-md-3"><h3>{{ this.room["participants"].length }}</h3></div>
+        <div class="ranking-points col col-md-1">
           <button @click="goToRoom" class="btn btn-success">Unirse</button>
         </div>
       </div>
