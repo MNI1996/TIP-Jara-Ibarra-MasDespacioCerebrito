d3a78181cf3ad277b6f7377d831d1bb45c62d41b
Ivan <ivanjaratamargo@gmail.com>
2020-12-08 12:51:59 -0300

    add class for animation in SimpleCard.vue

--- frontend/src/components/SimpleCard.vue
+++ frontend/src/components/SimpleCard.vue
@@ -2,7 +2,7 @@
   <div @click="$emit('addCategory', dato)" class="col-4 col-md-3 center-card">
       <p>{{ dato }}</p>
     <div class="col-md-8 catCard" :class="{selected: isCatSelected()}">
-      <img :src=generateUrl(dato) alt="" class="img-fluid">
+      <img :src=generateUrl(dato) alt="" class="img-fluid heartbeat-category">
     </div>
   </div>
 
