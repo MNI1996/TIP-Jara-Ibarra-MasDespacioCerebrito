64572d307124d27f430b7bc8b2e48844170597d5
Ivan <ivanjaratamargo@gmail.com>
2020-12-11 22:51:15 -0300

    add avatarImages in UserCard.vue

--- frontend/src/components/UserCard.vue
+++ frontend/src/components/UserCard.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="user-card">
-    <img src="Images/Profile.png" alt="" class="img-fluid user-card-img">
-    <p>{{dato}}</p>
+    <img :src="`Images/avatars/${avatarImage}.png`" class="img-fluid user-card-img">
+    <p class="user-card-p">{{dato}}</p>
   </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+
 export default {
 name: "UserCard",
   props: {dato:String},
+  computed: {
+      ...mapGetters(["players"]),
+      avatarImage(){
+        let player = this.players.find(p => p._id===this.dato);
+        return player && player.avatar_image_name ? player.avatar_image_name : 'man';
+      }
+  }
 }
 </script>
